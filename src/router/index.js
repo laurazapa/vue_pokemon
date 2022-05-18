b3db994524d57a304090691ed441763da26d0e7f
Laura <laura_90bcn@hotmail.com>
2022-05-18 19:07:27 +0200

    protegir rutes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-// import store from '@/store'
+import store from '@/store'
 
 const routes = [
   {
@@ -25,45 +25,45 @@ const routes = [
   {
     path: '/pokedex',
     name: 'pokedex',
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.getLoggedin){
-    //     next();
-    //   } 
-    //   else next({ name: 'login' });
-    // },
+    beforeEnter: (to, from, next) => {
+      if (store.getters.getLoggedin){
+        next();
+      } 
+      else next({ name: 'login' });
+    },
     component: () => import(/* webpackChunkName: "Pokedex" */ '../views/PokedexView.vue')
   },
   {
     path: '/pokemon/:name',
     name: 'pokemon',
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.getLoggedin){
-    //     next();
-    //   } 
-    //   else next({ name: 'login' });
-    // },
+    beforeEnter: (to, from, next) => {
+      if (store.getters.getLoggedin){
+        next();
+      } 
+      else next({ name: 'login' });
+    },
     component: () => import(/* webpackChunkName: "Pokemon" */ '../views/PokemonView.vue')
   },
   {
     path: '/habitats',
     name: 'habitats',
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.getLoggedin){
-    //     next();
-    //   } 
-    //   else next({ name: 'login' });
-    // },
+    beforeEnter: (to, from, next) => {
+      if (store.getters.getLoggedin){
+        next();
+      } 
+      else next({ name: 'login' });
+    },
     component: () => import(/* webpackChunkName: "Pokedex" */ '../views/HabitatsView.vue')
   },
   {
     path: '/habitat/:name',
     name: 'habitat',
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.getLoggedin){
-    //     next();
-    //   } 
-    //   else next({ name: 'login' });
-    // },
+    beforeEnter: (to, from, next) => {
+      if (store.getters.getLoggedin){
+        next();
+      } 
+      else next({ name: 'login' });
+    },
     component: () => import(/* webpackChunkName: "Pokedex" */ '../views/HabitatView.vue')
   },
 ]
